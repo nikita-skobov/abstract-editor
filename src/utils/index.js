0d70d7e27d32ee09d0f869484b4332ab7c481a81
nikita-skobov <nskobov.websites@gmail.com>
2019-07-22 13:39:03 -0500

    adds option for default props for makeReactObject

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 export const has = Object.prototype.hasOwnProperty
 
-export function makeReactObject(Comp) {
+export function makeReactObject(Comp, useProps = {}) {
   if (typeof Comp === 'function') {
-    return <Comp />
+    return <Comp {...useProps} />
   }
 
   // it already is a react object
